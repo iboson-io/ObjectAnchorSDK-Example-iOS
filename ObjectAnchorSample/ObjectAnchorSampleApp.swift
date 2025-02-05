3e2b428ba5d62fc2f15b0a5621bd30c44bbc7677
Atul <atul@ibosoninnov.com>
2025-02-05 19:09:08 +0530

    Initial Commit

---
+++ ObjectAnchorSample/ObjectAnchorSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectAnchorSampleApp.swift
+//  ObjectAnchorSample
+//
+//  Created by Atul Vasudev A on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectAnchorSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
